3518f0567a7dea22b8526329371f20cea9e61c09
OteroSantiago <91058589+OteroSantiago@users.noreply.github.com>
2021-10-12 20:45:23 +0200

    Initial commit

---
+++ src/caracteresespeciales/CaracteresEspeciales.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package caracteresespeciales;
+
+/**
+ *
+ * @author Santiago
+ */
+public class CaracteresEspeciales {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        var centro = "CEEP";
+        System.out.println("Salto de línea: \n" + centro);
+        System.out.println("Tabulador: \t" + centro);
+        System.out.println("Retroceso: \b\b" + centro);
+        System.out.println("Comilla simple: \'" + centro);
+        System.out.println("Comilla doble: \"" + centro);
+    }
+    
+}
